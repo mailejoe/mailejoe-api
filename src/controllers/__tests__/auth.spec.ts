cac693beb24d7a9c840a87a87c2cec58c28cd1c1
Marc Fisher <mcfisher83@gmail.com>
2021-11-26 20:36:34 -0600

    Added test for when token doesn t match any user.

--- src/controllers/__tests__/auth.spec.ts
+++ src/controllers/__tests__/auth.spec.ts
@@ -1076,5 +1076,20 @@ describe('auth', () => {
       expect(mockResponse.status).toBeCalledWith(403);
       expect(json).toBeCalledWith({ error: `Unauthorized` });
     });
+
+    it(`should return a 403 if token does not match any existing user`, async () => {
+      mockRequest = {
+        body: { password: chance.string() },
+        query: { token: chance.string() },
+        ...mockRequest,
+      };
+
+      mockValue(findOne, MockType.Resolve, false);
+
+      await passwordReset(mockRequest as Request, mockResponse as Response);
+
+      expect(mockResponse.status).toBeCalledWith(403);
+      expect(json).toBeCalledWith({ error: `Unauthorized` });
+    });
   });
 });
