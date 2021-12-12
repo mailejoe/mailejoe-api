2ec7b58accaf4e8951cc15b3d9ac3fe4d8834b4d
Marc Fisher <mcfisher83@gmail.com>
2021-12-12 13:42:20 -0600

    Added Role/Permission exports to entity folder

--- src/entity/index.ts
+++ src/entity/index.ts
@@ -1,6 +1,8 @@
 export { AuditLog } from './AuditLog';
 export { Organization } from './Organization';
+export { Permission } from './Permission';
 export { RateLimit } from './RateLimit';
+export { Role } from './Role';
 export { Session } from './Session';
 export { User } from './User';
 export { UserAccessHistory } from './UserAccessHistory';
