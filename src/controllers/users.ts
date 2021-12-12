1b35dfbbabe0334432cce906885d1d36d86822c9
Marc Fisher <mcfisher83@gmail.com>
2021-12-12 12:13:40 -0600

    Working on outlining the API for user management.

---
+++ src/controllers/users.ts
@@ -0,0 +1,12 @@
+/*
+export async function fetchUsers(req: Request, res: Response) {
+  // filter, offset, limit, fields
+
+  // does the user have access?
+}
+*/
+
+// export async function fetchUser(req: Request, res: Response) {}
+// export async function createUser(req: Request, res: Response) {}
+// export async function updateUser(req: Request, res: Response) {}
+// export async function deleteUser(req: Request, res: Response) {}
